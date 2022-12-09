730f55510b2b41ea0c8d1ff5bf0973e22a4c5f34
LSystemus <info@louisbeke.xyz>
2022-12-09 22:12:26 +0100

    Updated map.js (location)

--- js/map.js
+++ js/map.js
@@ -1,10 +1,10 @@
-let lsmap = L.map('lsmap').setView([50.7068352341879, 4.24997673652956], 20);
+let lsmap = L.map('lsmap').setView([50.706806477737544, 4.25039255269548], 20);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(lsmap);
 
-let marker = L.marker([50.7068012340879, 4.24997673652956]).addTo(lsmap);
+let marker = L.marker([50.706806477737544, 4.25039255269548]).addTo(lsmap);
 
 marker.bindPopup("LSystemus<br>Dokter Spitaelslaan 438 <br> B-1502 Halle").openPopup();
