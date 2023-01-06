972a2a132d8bd7029e47460d85147a0cdd9d704d
Louis Beke <info@louisbeke.xyz>
2023-01-06 21:44:28 +0100

    up

--- js/login-page.js
+++ js/login-page.js
@@ -6,7 +6,7 @@ loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-
+    // change this to database
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
         location.reload();
